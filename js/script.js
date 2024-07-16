519747d6b6e33274b6c847b8b41174e84d58ddce
SaD-Pr0ger <sad.pr0ger@gmail.com>
2024-07-16 23:03:12 +0500

    products slider/card and categories

--- js/script.js
+++ js/script.js
@@ -31,14 +31,35 @@ inputSearch.addEventListener('keydown', toggleSearchBtn)
 let menuDropdown = document.querySelector('.bottom-header .dropdown')
 menuDropdown.addEventListener('click', toggleMenuHeaderDropdown)
 
-const swiper = new Swiper('.banner .slide-banner .swiper', {
+new Swiper('.banner .slide-banner .swiper', {
     slidesPerView: 1,
     pagination: {
         el: ".slide-banner-pagination",
         type: "fraction",
     },
     navigation: {
-        nextEl: ".slide-banner-button-next",
-        prevEl: ".slide-banner-button-prev",
+        nextEl: ".banner .slide-banner .navigation .slide-banner-button-next",
+        prevEl: ".banner .slide-banner .navigation .slide-banner-button-prev",
     },
 });
+
+let selectorParents = ['.athletic-category', '.street-category', '.sweden-category']
+selectorParents.forEach((className) => {
+    new Swiper(className + ' .products-slide .swiper', {
+        slidesPerView: 'auto',
+        watchOverflow: true,
+        navigation: {
+            nextEl: className + " .products-slide .navigation .slide-banner-button-next",
+            prevEl: className + " .products-slide .navigation .slide-banner-button-prev",
+        },
+    })
+})
+
+document.querySelectorAll('.product-card').forEach((element) => {
+    element.addEventListener('mouseover', (event) => {
+        element.querySelector('.add-to-favorite').classList.remove('not-visible')
+    })
+    element.addEventListener('mouseout', (event) => {
+        element.querySelector('.add-to-favorite').classList.add('not-visible')
+    })
+})
